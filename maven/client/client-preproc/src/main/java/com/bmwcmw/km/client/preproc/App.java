f2b6204a4370c19030136b5db9d98ea3f79f6142
CMWX230 <900930@gmail.com>
2015-03-03 23:47:05 +0100

    UPDATE project structure

---
+++ maven/client/client-preproc/src/main/java/com/bmwcmw/km/client/preproc/App.java
@@ -0,0 +1,13 @@
+package com.bmwcmw.km.client.preproc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
